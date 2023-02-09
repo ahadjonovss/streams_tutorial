a659b6c118c23e0d61d0fef4ef641baf5a455ab9
Samandar <samandarahadjonov@gmail.com>
2023-02-09 14:44:40 +0500

    some

--- lib/cubit/ai_cubit/ai_cubit.dart
+++ lib/cubit/ai_cubit/ai_cubit.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:streams_tutorial/data/models/question_model.dart';
 import 'package:streams_tutorial/data/repositories/storage_repository.dart';
 import 'package:streams_tutorial/utils/get_it.dart';
 
@@ -14,10 +15,27 @@ class AiCubit extends Cubit<AiState> {
 
   addAnswerToStream(){}
 
-  findAnswer(String question){
+  Future<String?> findAnswer(String question) async {
     bool isFound = false;
     for(int i =0;isFound==true;i++){
-      getIt<StorageRepository>();
+      QuestionModel? result =  await getModel(i);
+      if(result!=null && result.question==question){
+        isFound = true;
+        return result.answer;
+      }else if(result==null){
+        return "Sorry but I don't know";
+      }
+    }
+    return null;
+  }
+
+  Future<QuestionModel?> getModel(int key) async {
+    String? result =  await getIt<StorageRepository>().getQuestion(key);
+    if(result!=null){
+      return QuestionModel(answer: result.split("|")[1], question: result.split("|")[0]);
+    }
+    else{
+      return null;
     }
   }
 }
