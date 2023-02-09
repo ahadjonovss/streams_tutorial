d2baa739b17ac96f0dfd7fc5a039387559e25d0c
Samandar <samandarahadjonov@gmail.com>
2023-02-09 14:01:21 +0500

    some

--- lib/ui/add_question_page.dart
+++ lib/ui/add_question_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
 class AddQuestionPage extends StatelessWidget {
-  const AddQuestionPage({Key? key}) : super(key: key);
+  AddQuestionPage({Key? key}) : super(key: key);
+
+  TextEditingController answer = TextEditingController();
+  TextEditingController question = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        alignment: Alignment.center,
+        child: Column(
+          children:  [
+            TextField(
+              controller: question,
+              decoration: const InputDecoration(
+                hintText: "Your Question"
+              ),
+            ),
+            TextField(
+              controller: answer,
+              decoration:  const InputDecoration(
+                hintText: "Your answer"
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: () {
+
+            }, child: const Text("Save"))
+          ],
+        ),
+      ),
+    );
   }
 }
